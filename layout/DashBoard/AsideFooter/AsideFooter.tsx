75d13f90da712dcff1804fb655ff95def5cf9644
freedombusinesssummit-stack <freedom.business.summit@gmail.com>
2026-05-06 21:35:26 +0800

    Add files via upload

--- layout/DashBoard/AsideFooter/AsideFooter.tsx
+++ layout/DashBoard/AsideFooter/AsideFooter.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useUserStore } from '@/store/useUserStore';
+import { useRouter } from 'next/navigation';
 
 type AsideFooterProps = {
 	user?: any;
@@ -6,6 +8,7 @@ type AsideFooterProps = {
 };
 
 const AsideFooter = ({ user, profile }: AsideFooterProps) => {
+	const router = useRouter();
 	const companyName = profile?.company_name || 'Partner Account';
 
 	const initials = companyName
@@ -15,23 +18,53 @@ const AsideFooter = ({ user, profile }: AsideFooterProps) => {
 		.slice(0, 2)
 		.toUpperCase();
 
+	const { logout } = useUserStore();
+
+	const logoutFunction = () => {
+		logout();
+		router.push('/login');
+	};
+
 	return (
-		<div className='flex items-center justify-between bg-gray-50 rounded-lg p-3 cursor-pointer hover:bg-gray-100'>
-			<div className='flex items-center gap-3'>
-				<div className='w-9 h-9 bg-black text-white flex items-center justify-center rounded-md text-sm font-semibold'>
-					{initials}
-				</div>
+		<div className='relative group'>
+			{/* MAIN BUTTON */}
+			<div className='flex items-center justify-between bg-gray-50 rounded-lg p-3 cursor-pointer hover:bg-gray-100 transition'>
+				<div className='flex items-center gap-3'>
+					<div className='w-9 h-9 bg-black text-white flex items-center justify-center rounded-md text-sm font-semibold'>
+						{initials}
+					</div>
 
-				<div>
-					<div className='text-sm font-medium text-gray-900'>{companyName}</div>
+					<div>
+						<div className='text-sm font-medium text-gray-900 break-words max-w-[127px]'>
+							{companyName}
+						</div>
 
-					<div className='text-xs text-gray-500'>
-						{user?.email || 'No email'}
+						<div className='text-xs text-gray-500 break-words max-w-[127px]'>
+							{user?.email || 'No email'}
+						</div>
 					</div>
 				</div>
+
+				<span className='text-gray-400'>›</span>
 			</div>
 
-			<span className='text-gray-400'>›</span>
+			{/* DROPDOWN */}
+			<div className='absolute left-full bottom-0 ml-2 w-52 bg-white border border-gray-200 rounded-sm shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50 overflow-hidden'>
+				<button
+					onClick={logoutFunction}
+					className='w-full text-left px-4 py-3 text-sm text-red-500 hover:bg-gray-50 transition cursor-pointer'
+				>
+					Log Out
+				</button>
+
+				<button className='w-full text-left px-4 py-3 text-sm text-gray-700 hover:bg-gray-50 transition cursor-pointer'>
+					Account Settings
+				</button>
+
+				<button className='w-full text-left px-4 py-3 text-sm text-gray-700 hover:bg-gray-50 transition cursor-pointer'>
+					Billing
+				</button>
+			</div>
 		</div>
 	);
 };
